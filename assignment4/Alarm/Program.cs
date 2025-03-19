fa429bc63755e9d356fbb35078b793f6375ff770
fivekilo <3060882350@qq.com>
2025-03-19 22:48:25 +0800

    assignment4

---
+++ assignment4/Alarm/Program.cs
@@ -0,0 +1,45 @@
+﻿class clock
+{
+    public delegate void AlarmHandler(object sender);
+    public event AlarmHandler tick;
+    public event AlarmHandler alarm;
+    public void Tick()
+    {
+        tick(this);
+    }
+    public void Alarm()
+    {
+        alarm(this);
+    }
+}
+
+class timer
+{
+    public clock clock1 = new clock();
+    public timer()
+    {
+        clock1.tick += TickHandler;
+        clock1.alarm += AlarmHandler;
+    }
+    void TickHandler(object sender)
+    {
+        Console.WriteLine("Tick");
+    }
+    void AlarmHandler(object sender)
+    {
+        Console.WriteLine("Alarm");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        timer timer1 = new timer();
+        timer1.clock1.Tick();
+        timer1.clock1.Tick();
+        timer1.clock1.Tick();
+        timer1.clock1.Alarm();
+        timer1.clock1.Tick();
+    }
+}
---
+++ assignment4/genericList/Program.cs
@@ -0,0 +1,70 @@
+﻿
+public class Node<T>
+{
+    public Node<T> Next { get; set; }
+    public T Data { get; set; }
+
+    public Node(T t)
+    {
+        Next = null;
+        Data = t;
+    }
+}
+public class GenericList<T>
+{
+    private Node<T> head;
+    private Node<T> tail;
+
+    public GenericList()
+    {
+        tail = head = null;
+    }
+    public Node<T> Head
+    {
+        get=> head;
+    }
+    public void Add(T t)
+    {
+        Node<T> n = new Node<T>(t);
+        if (tail == null)
+        {
+            head=tail= n;
+        }
+        else
+        {
+            tail.Next = n;
+            tail= n;
+        }
+    }
+    public void ForEach(Action<T> action)
+    {
+        Node<T>pointer= head;
+        while(pointer != null)
+        {
+            action(pointer.Data);
+            pointer=pointer.Next;
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        GenericList<int> list = new GenericList<int>();
+        list.Add(1);
+        list.Add(2);
+        list.Add(4);
+        list.Add(0);
+        list.ForEach(x=>Console.WriteLine(x));
+        int max=int.MinValue;
+        list.ForEach(x => { if (x > max) max = x; });
+        Console.WriteLine($"最大值:{max}");
+        int min=int.MaxValue;
+        list.ForEach(x => { if (x < min) min = x; });
+        Console.WriteLine($"最小值:{min}");
+        int sum = 0;
+        list.ForEach(x => { sum += x; });
+        Console.WriteLine($"总和:{sum}");
+    }
+}
